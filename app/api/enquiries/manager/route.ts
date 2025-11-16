63a0c228d3c97718e1ef32db41e88eec12cb1b62
itbienvenu <bienvenugashema@gmail.com>
2025-11-16 21:28:12 +0200

    Creating the endpoint for endoint to get requested enquiries

---
+++ app/api/enquiries/manager/route.ts
@@ -0,0 +1,17 @@
+import { EnquiriesModel } from "@/src/lib/models/enquiry.model";
+import { NextResponse, NextRequest } from "next/server";
+import { roleMiddleware } from "@/src/middleware/auth";
+
+
+export async function GET(req: NextRequest) {
+    const authResult = await roleMiddleware(req, ["ADMIN", "EXECUTIVE"]);
+    if (authResult instanceof NextResponse) return authResult;
+    const { user } = authResult;
+    try {
+        const enquiries = await EnquiriesModel.getAllEnquiries();
+        return NextResponse.json(enquiries, { status: 200 });
+        } catch (err: any) {
+        console.error(err);
+        return NextResponse.json({ error: err.message }, { status: 400 });  
+        }
+}
