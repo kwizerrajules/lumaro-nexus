8666976c582661dad81e15f7b1bf7f30da3c5ed8
itbienvenu <bienvenugashema@gmail.com>
2025-11-21 11:41:23 +0200

    FIx: esolving the missing modelus

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    serverComponentsExternalPackages: ['jsdom', 'parse5'],
+  },
 };
 
 module.exports = nextConfig;
-
